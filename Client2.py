6c82c4d18d23fe469d6730d79a7a2175d47f7b13
Gu256096y <guy.ravon2560@gmail.com>
2025-08-26 11:48:34 +0300

    Update Client2.py

--- Client2.py
+++ Client2.py
@@ -1,4 +1,4 @@
-#Guy Rav On - 315044743
+#Guy Rav On
 #A main function for a client
 
 from ClientRequestHandler import *
@@ -73,4 +73,5 @@ def interactive_client():
             print("Invalid choice. Please try again.")
 
 if __name__ == "__main__":
+
     interactive_client()
